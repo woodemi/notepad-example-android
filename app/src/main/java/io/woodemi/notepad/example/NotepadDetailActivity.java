88b40235e455defc2fa13f2e8af724ae6830bc6e
wangkun <wangkun@woodemi.com>
2020-01-14 15:32:31 +0800

    Add `NotepadClient` to `NotepadDetailActivity`
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- app/src/main/java/io/woodemi/notepad/example/NotepadDetailActivity.java
+++ app/src/main/java/io/woodemi/notepad/example/NotepadDetailActivity.java
@@ -3,12 +3,25 @@ package io.woodemi.notepad.example;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import io.woodemi.notepad.ConnectionState;
+import io.woodemi.notepad.NotePenPointer;
+import io.woodemi.notepad.NotepadClient;
 import io.woodemi.notepad.NotepadConnector;
+import io.woodemi.notepad.NotepadConnectorCallback;
+import io.woodemi.notepad.NotepadMessage;
 import io.woodemi.notepad.NotepadScanResult;
 
 public class NotepadDetailActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "NotepadDetailActivity";
+
     public static final String EXTRA_SCAN_RESULT = "scan_result";
 
     private NotepadScanResult notepadScanResult;
@@ -21,6 +34,8 @@ public class NotepadDetailActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_notepad_detail);
         findViewById(R.id.connect).setOnClickListener(this);
         findViewById(R.id.disconnect).setOnClickListener(this);
+
+        NotepadConnector.callback = connectionCallback;
     }
 
     @Override
@@ -34,4 +49,34 @@ public class NotepadDetailActivity extends AppCompatActivity implements View.OnC
                 break;
         }
     }
+
+    private NotepadConnectorCallback connectionCallback = (notepadClient, state) -> {
+        runOnUiThread(() -> Toast.makeText(this, state.getClass().getSimpleName(), Toast.LENGTH_SHORT).show());
+        if (state instanceof ConnectionState.Disconnected) {
+            if (this.notepadClient != null) {
+                this.notepadClient.setCallback(null);
+            }
+            this.notepadClient = null;
+        } else if (state instanceof ConnectionState.Connected) {
+            if (this.notepadClient != null) {
+                this.notepadClient.setCallback(null);
+            }
+            this.notepadClient = notepadClient;
+            this.notepadClient.setCallback(this.clientCallback);
+        }
+    };
+
+    private NotepadClient notepadClient;
+
+    private NotepadClient.Callback clientCallback = new NotepadClient.Callback() {
+        @Override
+        public void handlePointer(@NotNull List<NotePenPointer> list) {
+            Log.d(TAG, "handlePointer " + list.size());
+        }
+
+        @Override
+        public void handleEvent(@NotNull NotepadMessage notepadMessage) {
+            Log.d(TAG, "handleEvent " + notepadMessage);
+        }
+    };
 }
